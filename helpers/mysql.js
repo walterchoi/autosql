aaed2f0d679e9902efe397ace0978a6d8f911fcc
Walter <w@walterchoi.com>
2020-04-16 16:21:22 +1000

    create table sql query made (override not done)

--- helpers/mysql.js
+++ helpers/mysql.js
@@ -40,7 +40,7 @@ var exports = {
                             if(repeat_number) {repeat_number = repeat_number + 1}
                             else {repeat_number = 1}
                             if (repeat_number < max_repeat) {
-                                var nested_query = await query(pool, sql_query, repeat_number)
+                                var nested_query = await run_query(pool, sql_query, repeat_number)
                                 if(!nested_query.err) {resolve ({err: nested_query.err, 
                                 results: nested_query.results
                                 })}
