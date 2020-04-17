cb704cb617b980cf0e9f63b9d8e93006b2e2d4c7
Walter <w@walterchoi.com>
2020-04-17 15:42:43 +1000

    compare two headers

--- index.js
+++ index.js
@@ -505,7 +505,7 @@ async function convert_table_description (table_description) {
         var old_length = table_desc[c]['LENGTH']
         var nullable = table_desc[c].IS_NULLABLE
         console.log(old_length)
-        if(old_length.contains(',')) {
+        if(old_length.includes(',')) {
             var decimal = old_length.toString().split(",")[1].length
             old_length = old_length.toString().split(",")[0].length
         }
