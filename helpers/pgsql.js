c8c187d4942c269900f13741ae13ee03b73d9463
Walter <w@walterchoi.com>
2020-05-12 16:41:54 +1000

    release on error pg

--- helpers/pgsql.js
+++ helpers/pgsql.js
@@ -91,6 +91,7 @@ var exports = {
                         if(repeat_number) {repeat_number = repeat_number + 1}
                         else {repeat_number = 1}
                         if (repeat_number < max_repeat) {
+                        release()
                         var nested_query = await exports.run_query(config, sql_query, repeat_number)
                         if(!nested_query.err) {resolve ({err: nested_query.err, 
                             results: nested_query.results
