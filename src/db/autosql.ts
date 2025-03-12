9b68ec8b8f69d196e3fdcab0d8cd24b969464dc7
Walter Choi <w@walterchoi.com>
2025-03-12 23:53:41 +1100

    start of autosql function

--- src/db/autosql.ts
+++ src/db/autosql.ts
@@ -1,6 +1,7 @@
 import { MySQLDatabase } from "./mysql";
 import { PostgresDatabase } from "./pgsql";
 import { InsertResult } from "../config/types";
+import { getMetaData } from "../helpers/metadata";
 
 export class AutoSQLHandler {
     private db: MySQLDatabase | PostgresDatabase;
@@ -10,7 +11,9 @@ export class AutoSQLHandler {
     }
 
     async execute(table: string, data: Record<string, any>[]): Promise<InsertResult> {
-
+        const currentMetaDataQuery = this.db.getTableMetaDataQuery(this.db.getConfig().schema || this.db.getConfig().database || "", table)
+        const currentMetaData = this.db.runQuery(currentMetaDataQuery);
+        const newMetaData = getMetaData(this.db.getConfig(), data)
         const start = this.db.startDate;
         const affectedRows = 0
         const end = new Date;
